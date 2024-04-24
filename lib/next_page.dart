70c69cb8249adb7f8b427c46363e0a557d8077a9
jumpei arai <jumpei0924@gmail.com>
2024-04-24 12:20:48 +0900

    listの追加

---
+++ lib/next_page.dart
@@ -0,0 +1,13 @@
+class NextPage extends StatefulWidget {
+  const NextPage({super.key});
+
+  @override
+  State<NextPage> createState() => _NextPageState();
+}
+
+class _NextPageState extends State<NextPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
